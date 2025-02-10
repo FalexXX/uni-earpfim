8115c28f525d63eddee3780dcfdb324e5d0c2a25
fabrizio <fabri.lveintimilla@gmail.com>
2025-02-10 14:49:04 -0500

    Modificación de archivos JavaScript

--- Login/envio.js
+++ Login/envio.js
@@ -8,42 +8,59 @@ formulario.addEventListener("submit", function (event) {
   console.log(email);
   console.log(contra);
   try {
-    fetch('../json/usuarios.json') 
-      .then(response => response.json()) 
-      .then(data => {
-        let usuarioEncontrado = data.find(user => user.email === email && user.password === contra);
-        
-        if (usuarioEncontrado) {
-          localStorage.setItem('id', usuarioEncontrado.id);
-          localStorage.setItem('codigo', usuarioEncontrado.codigo);
-          localStorage.setItem('nombre', usuarioEncontrado.nombre);
+    fetch("../json/usuarios.json")
+      .then((response) => response.json())
+      .then((data) => {
+        let usuarioEncontrado = data.find(
+          (user) => user.email === email && user.password === contra
+        );
 
-          console.log(localStorage.getItem('id'));
-          console.log(localStorage.getItem('codigo'));
+        if (usuarioEncontrado) {
+          localStorage.setItem("id", usuarioEncontrado.id);
+          localStorage.setItem("email", usuarioEncontrado.email);
+          localStorage.setItem("password", usuarioEncontrado.password);
+          localStorage.setItem("nombre", usuarioEncontrado.nombre);
+          localStorage.setItem("codigo", usuarioEncontrado.codigo);
+          localStorage.setItem("dni", usuarioEncontrado.dni);
+          localStorage.setItem("sexo", usuarioEncontrado.sexo);
+          localStorage.setItem("fecha_nacimiento", usuarioEncontrado.fecha_nacimiento);
+          localStorage.setItem("celular", JSON.stringify(usuarioEncontrado.celular));
+          localStorage.setItem("telefono_fijo", JSON.stringify(usuarioEncontrado.telefono_fijo));
+          localStorage.setItem("fecha_matrimonio", usuarioEncontrado.fecha_matrimonio);
+          localStorage.setItem("emails_alternativos",JSON.stringify(usuarioEncontrado.emails_alternativos));
+          localStorage.setItem("pais_nacimiento",usuarioEncontrado.pais_nacimiento);
+          localStorage.setItem("departamento_nacimiento",usuarioEncontrado.departamento_nacimiento);
+          localStorage.setItem("provincia_nacimiento",usuarioEncontrado.provincia_nacimiento);
+          localStorage.setItem("distrito_nacimiento",usuarioEncontrado.distrito_nacimiento);
+          localStorage.setItem("domicilio", JSON.stringify(usuarioEncontrado.domicilio));
+          localStorage.setItem("datos_laborales", JSON.stringify(usuarioEncontrado.datos_laborales));
+          
+          console.log(localStorage.getItem("id"));
+          console.log(localStorage.getItem("codigo"));
 
           Swal.fire({
-            icon: 'success',
-            title: 'Bienvenido',
+            icon: "success",
+            title: "Bienvenido",
             text: `Hola, ${usuarioEncontrado.nombre}!`,
-            backdrop: false
+            backdrop: false,
           }).then(() => {
-            window.location.href = '../index.html';
+            window.location.href = "../index.html";
           });
         } else {
           Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Credenciales incorrectas',
-            backdrop: false
+            icon: "error",
+            title: "Oops...",
+            text: "Credenciales incorrectas",
+            backdrop: false,
           }).then(() => {
-            window.location.href = 'login.html';
+            window.location.href = "login.html";
           });
         }
       });
   } catch (error) {
-    alert('Ocurrió un error al cargar los datos');
+    alert("Ocurrió un error al cargar los datos");
     console.error(error);
   } finally {
-    console.log('Fin de la solicitud');
+    console.log("Fin de la solicitud");
   }
 });
--- script.js
+++ script.js
@@ -1,93 +1,108 @@
-
 // TABLA LATERAL
 let lisElements = document.querySelectorAll(".list__button--click");
 
 lisElements.forEach((element) => {
-    element.addEventListener("click", (event) => {
-        event.preventDefault();
-        element.classList.toggle("arrow");
+  element.addEventListener("click", (event) => {
+    event.preventDefault();
+    element.classList.toggle("arrow");
 
-        let menu = element.nextElementSibling;
+    let menu = element.nextElementSibling;
 
-        // Alternar el submenú
-        if (menu.style.maxHeight === "0px" || menu.style.maxHeight === "") {
-            menu.style.maxHeight = `${menu.scrollHeight}px`;
-        } else {
-            menu.style.maxHeight = "0px";
-        }
-    });
+    // Alternar el submenú
+    if (menu.style.maxHeight === "0px" || menu.style.maxHeight === "") {
+      menu.style.maxHeight = `${menu.scrollHeight}px`;
+    } else {
+      menu.style.maxHeight = "0px";
+    }
+  });
 });
 
 // MENU-LATERAL
 let menu_lateral = document.querySelector(".menu-lateral");
 let btn_menu = document.getElementById("sidebar");
-btn_menu.addEventListener('click', function(event) {
-    event.stopPropagation();
-    menu_lateral.classList.toggle("closed");
+btn_menu.addEventListener("click", function (event) {
+  event.stopPropagation();
+  menu_lateral.classList.toggle("closed");
 });
 
-document.addEventListener('click', function(event) {
-    const click_dentro = menu_lateral.contains(event.target);
-    const click_fuera = btn_menu.contains(event.target);
+document.addEventListener("click", function (event) {
+  const click_dentro = menu_lateral.contains(event.target);
+  const click_fuera = btn_menu.contains(event.target);
 
-    if (window.innerWidth <= 768) {
-        // Cierra toda la barra lateral si se hace clic fuera
-        if (!click_dentro && !click_fuera) {
-            menu_lateral.classList.add('closed');
-        }
-
-        // Cierra el submenú si se hace clic fuera
-        lisElements.forEach((element) => {
-            let menu = element.nextElementSibling;
-            if (!element.contains(event.target) && menu.style.maxHeight !== "0px") {
-                menu.style.maxHeight = "0px";
-                element.classList.remove("arrow");
-            }
-        });
+  if (window.innerWidth <= 768) {
+    // Cierra toda la barra lateral si se hace clic fuera
+    if (!click_dentro && !click_fuera) {
+      menu_lateral.classList.add("closed");
     }
-});
 
+    // Cierra el submenú si se hace clic fuera
+    lisElements.forEach((element) => {
+      let menu = element.nextElementSibling;
+      if (!element.contains(event.target) && menu.style.maxHeight !== "0px") {
+        menu.style.maxHeight = "0px";
+        element.classList.remove("arrow");
+      }
+    });
+  }
+});
 
 //JSON GENERAL
 document.addEventListener("DOMContentLoaded", function () {
-         let nombreUsuario = localStorage.getItem('nombre');
-         let idUsuario = localStorage.getItem('id');
-         let codigoUsuario = localStorage.getItem('codigo');
+    
+  let idUsuario = localStorage.getItem("id");
+  let emailUsuario = localStorage.getItem("email");
+  let nombreUsuario = localStorage.getItem("nombre");
+  let codigoUsuario = localStorage.getItem("codigo");
+  let dniUsuario = localStorage.getItem("dni");
+  let sexoUsuario = localStorage.getItem("sexo");
+  let fechaNacimiento = localStorage.getItem("fecha_nacimiento");
+  let celularUsuario = JSON.parse(localStorage.getItem("celular"));
+  let telefonoFijo = JSON.parse(localStorage.getItem("telefono_fijo"));
+  let fechaMatrimonio = localStorage.getItem("fecha_matrimonio");
+  let emailsAlternativos = JSON.parse(localStorage.getItem("emails_alternativos"));
+  let paisNacimiento = localStorage.getItem("pais_nacimiento");
+  let departamentoNacimiento = localStorage.getItem("departamento_nacimiento");
+  let provinciaNacimiento = localStorage.getItem("provincia_nacimiento");
+  let distritoNacimiento = localStorage.getItem("distrito_nacimiento");
+  let domicilioUsuario = JSON.parse(localStorage.getItem("domicilio"));
+  let datosLaborales = JSON.parse(localStorage.getItem("datos_laborales"));
 
-         let nombreDisplays = document.querySelectorAll('#nombre-usuario');
-         let imagenProfesor = document.querySelector('.imagen-perfil');
-         let codigoProfesor = document.getElementById('codigo-perfil');
-         
-         nombreDisplays.forEach(elemento => {
-           elemento.textContent = nombreUsuario ? `${nombreUsuario}` : 'Bienvenido, Invitado';
-         });
+  let nombreDisplays = document.getElementById("#nombre-usuario");
+  let imagenProfesor = document.querySelector(".imagen-perfil");
+  let codigoProfesor = document.getElementById("codigo-perfil");
 
-         if( `${idUsuario}` == 1 ){    
-            codigoProfesor.textContent = `${codigoUsuario}`;
-            imagenProfesor.innerHTML = `<img src="../img/imagen-hombre.jpg" alt="Foto de perfil">`;
-         }else if( `${idUsuario}` == 2 ){
-            codigoProfesor.textContent = `${codigoUsuario}`;
-            imagenProfesor.innerHTML = `<img src="../img/imagen-mujer.jpg" alt="Foto de perfil">`;
-         }else{
-            console.log("Error al cargar los datos");
-         }
+  nombreDisplays.forEach((elemento) => {
+    elemento.textContent = nombreUsuario
+      ? `${nombreUsuario}`
+      : "Bienvenido, Invitado";
+  });
 
-       });
+  if (`${idUsuario}` == 1) {
+    codigoProfesor.textContent = `${codigoUsuario}`;
+    imagenProfesor.innerHTML = `<img src="../img/imagen-hombre.jpg" alt="Foto de perfil">`;
+  } else if (`${idUsuario}` == 2) {
+    codigoProfesor.textContent = `${codigoUsuario}`;
+    imagenProfesor.innerHTML = `<img src="../img/imagen-mujer.jpg" alt="Foto de perfil">`;
+  } else {
+    console.log("Error al cargar los datos");
+  }
+});
 
+//PAGINACIÓN
+document.querySelectorAll(".page-item").forEach((item) => {
+  item.addEventListener("click", function (e) {
+    e.preventDefault;
 
-     //PAGINACIÓN
-     document.querySelectorAll(".page-item").forEach(item=>{
-        item.addEventListener('click', function(e){
-            e.preventDefault;
+    document
+      .querySelectorAll(".page-item")
+      .forEach((el) => el.classList.remove("active"));
+    document
+      .querySelectorAll(".contenido")
+      .forEach((section) => section.classList.remove("active"));
 
-            document.querySelectorAll(".page-item").forEach(el=>el.classList.remove('active'));
-            document.querySelectorAll(".contenido").forEach(section=>section.classList.remove('active'));
+    this.classList.add("active");
 
-            this.classList.add('active');
-
-            const targetId = this.id.replace('-btn', ''); 
-            document.getElementById(targetId).classList.add('active');
-
-        })
-     })
-   
+    const targetId = this.id.replace("-btn", "");
+    document.getElementById(targetId).classList.add("active");
+  });
+});
